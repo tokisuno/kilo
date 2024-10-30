b689c155e903b9f0a50bd5ccb6f7820a647bab34
tokisuno <lucaswmei@gmail.com>
2024-10-30 08:46:14 -0400

    up to step 34 with some major issues

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # kilo
-* [Guide to Rewrite Kilo](https://viewsourcecode.org/snaptoken/kilo/03.rawInputAndOutput.html)
+* [Guide to Rewrite Kilo](https://viewsourcecode.org/snaptoken/kilo/03.#wInputAndOutput.html)
+
+# To Fix
+* For some reason after step 33, my terminal stops sizing things correctly.
--- kilo.c
+++ kilo.c
@@ -1,9 +1,8 @@
-/* #include <ctype.h> */
-#include <asm-generic/ioctls.h>
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -72,30 +71,32 @@ void editorProcessKeypress() {
 }
 
 int getCursorPosition(int *rows, int *cols) {
-    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+    char buf[32];
+    unsigned int i = 0;
 
-    printf("\r\n");
-    char c;
+    if ( write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
 
-    while (read(STDIN_FILENO, &c, 1) == 1) {
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
+    while (i<sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
     }
+    buf[i] = '\0';
 
-    editorReadKey();
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
 
-    return -1;
+    /* editorReadKey(); */
+
+    return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
     // TIOCSWINSZ
-    // Terminal I/O Control Get WINdow SiZe                          (wtf...)
-    if (1 || ioctl(STDOUT_FILENO, TIOCSWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    // Terminal I/O Control Get WINdow SiZe                     (wtf...)
+    if (ioctl(STDOUT_FILENO, TIOCSWINSZ, &ws) == -1 || ws.ws_col == 0) {
         if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
         return getCursorPosition(rows, cols);
 
@@ -106,23 +107,51 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
-/*** OUTPUT ***/
+/*** APPEND BUFFER ***/
+struct abuf {
+    char *b;
+    int len;
+};
 
-void editorDrawRows() {
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab-> len + len);
+
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
+/*** OUTPUT ***/
+void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        abAppend(ab, "~", 1);
+        if (y < E.screenrows - 1) {
+            abAppend(ab, "\r\n", 2);
+        }
     }
 }
 
 
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H",  3);
+    struct abuf ab = ABUF_INIT;
 
-    editorDrawRows();
+    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H",  3);
 
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    editorDrawRows(&ab);
+
+    abAppend(&ab, "\x1b[H", 3);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 
